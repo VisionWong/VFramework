1e1613011e5559154e68f058f3f5eb96118c13ec
VisionWong <302668321@qq.com>
2020-04-30 23:55:21 +0800

    场景加载模块

--- Assets/Scripts/VFramework/Scene/SceneMgr.cs
+++ Assets/Scripts/VFramework/Scene/SceneMgr.cs
@@ -1,21 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace VFramework
 {
-	public class SceneMgr : MonoBehaviour
+    /// <summary>
+    /// 场景管理器，负责同步异步加载场景
+    /// </summary>
+	public class SceneMgr : MonoSingleton<SceneMgr>
 	{
-	    // Start is called before the first frame update
-	    void Start()
-	    {
-	        
-	    }
-	
-	    // Update is called once per frame
-	    void Update()
-	    {
-	        
-	    }
-	}
+        /// <summary>
+        /// 同步加载场景，加载完后执行fun
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="fun"></param>
+	    public void LoadScene(string name, Action fun)
+        {
+            SceneManager.LoadScene(name);
+            fun?.Invoke();
+        }
+
+        /// <summary>
+        /// 异步加载场景，加载完执行回调函数
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="callback"></param>
+        public void LoadSceneAsync(string name, Action callback)
+        {
+            StartCoroutine(ReallyLoadSceneAsync(name, callback));          
+        }
+
+        private IEnumerator ReallyLoadSceneAsync(string name, Action callback)
+        {
+            AsyncOperation ao = SceneManager.LoadSceneAsync(name);
+            while (!ao.isDone)
+            {
+                //可通过消息中心发布事件，使得外界能够获取progress
+                yield return ao.progress;
+            }
+            callback?.Invoke();
+        }
+    }
 }
